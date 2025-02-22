29370ba7fe23597884b0ccd14c1d89e9c56cf3e3
QuaziAdnanHaque <126820867+QuaziAdnanHaque@users.noreply.github.com>
2025-02-22 15:30:38 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Adnan"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "adnan"
+
+let booleanIsLoogedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"adnan"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
