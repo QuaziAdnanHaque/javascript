e82c1aab50e622851a82fb0f7df809a243454fb3
QuaziAdnanHaque <126820867+QuaziAdnanHaque@users.noreply.github.com>
2025-03-24 10:37:05 +0000

    commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggeddIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+const bigNumber = 423423423423424242423424n 
+
+
+// Refence (Non Premitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Adnan",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Meow");
+    
+}
+
+// console.log(typeof bigNumber);
+console.log(typeof heros);
