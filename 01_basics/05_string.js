4b554a204ee2e309f866bb20aa457e7adb1825d8
QuaziAdnanHaque <haquequaziadnan@gmail.com>
2025-04-11 20:04:34 +0530

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,27 @@
+const name = "adnan"
+const repoCount = 3
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Valorant')
+
+// console.log(gameName[4]);
+
+// console.log(gameName.indexOf('a'));
+
+// console.log(gameName.substring(0,5));
+
+// console.log(gameName.slice(-6, 4));
+
+const newString = "   ADNAN   "
+
+// console.log(newString);
+// console.log(newString.trim());
+
+const mamma = "Adnan-is-a-good-boy"
+
+// console.log(mamma.replace('good', 'mamma`s'));
+
+console.log(mamma.split('-'));
