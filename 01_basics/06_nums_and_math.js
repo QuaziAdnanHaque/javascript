55803c3f8d8cfac03557fcbc60f5a20b2eeb2617
QuaziAdnanHaque <haquequaziadnan@gmail.com>
2025-04-13 11:42:18 +0530

    Create 06_nums_and_math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score  = 400
+// console.log(score);
+
+
+const balance = new Number(100.1213213141)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.8434
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-8));
+// console.log(Math.round(3.7));
+// console.log(Math.ceil(3.7));
+// console.log(Math.floor(3.7));
+// console.log(Math.min(3, 5, 7, 9));
+// console.log(Math.max(3, 5, 7, 9));
+
+// console.log(Math.random());
+// console.log(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
