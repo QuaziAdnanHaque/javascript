f1943817a1aa698783efdee723edeb1c79e583ca
QuaziAdnanHaque <haquequaziadnan@gmail.com>
2025-04-22 15:03:49 +0530

    Create 03_objects.js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+// singleton
+
+//Object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Adnan",
+    "Full name": "Quazi Adnan Haque",
+    [mySym]: "myKey1",
+    age: 21,
+    location: "Kolkata",
+    email: "haquequaziadnan@gmail.com",
+    isLogedIn: false,
+    lastLoginDays: ["Monday", "Wednesday"]
+}
+// console.log(JsUser.age);
+// console.log(JsUser["email"]);
+// console.log(JsUser["Full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "adnan@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email = "adnanhaque@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JsUser");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JsUser, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
